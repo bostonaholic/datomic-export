15bef5920f73fa6a571801a6a167cefc62ff0be9
Matthew Boston <matthew.boston@gmail.com>
2016-03-06 15:40:01 -0700

    datomic attributes should be a set

--- src/datomic_export/attributes_filterer.clj
+++ src/datomic_export/attributes_filterer.clj
@@ -2,23 +2,23 @@
   (:require [clojure.set :as set]
             [datomic.api :as d]))
 
-(def datomic-attributes '(:db/add :db/cardinality :db/code :db/doc :db/excise :db/fn :db/fulltext
-                                  :db/ident :db/index :db/isComponent :db/lang :db/noHistory :db/retract
-                                  :db/txInstant :db/unique :db/valueType
-                                  :db.alter/attribute
-                                  :db.bootstrap/part
-                                  :db.cardinality/many :db.cardinality/one
-                                  :db.excise/attrs :db.excise/before :db.excise/beforeT
-                                  :db.fn/cas :db.fn/retractEntity
-                                  :db.install/attribute :db.install/function :db.install/partition :db.install/valueType
-                                  :db.lang/clojure :db.lang/java
-                                  :db.part/db :db.part/tx :db.part/user
-                                  :db.sys/partiallyIndexed :db.sys/reId
-                                  :db.type/bigdec :db.type/bigint :db.type/boolean :db.type/bytes :db.type/double
-                                  :db.type/float :db.type/fn :db.type/instant :db.type/keyword :db.type/long :db.type/ref
-                                  :db.type/string :db.type/uri :db.type/uuid
-                                  :db.unique/identity :db.unique/value
-                                  :fressian/tag))
+(def datomic-attributes #{:db/add :db/cardinality :db/code :db/doc :db/excise :db/fn :db/fulltext
+                          :db/ident :db/index :db/isComponent :db/lang :db/noHistory :db/retract
+                          :db/txInstant :db/unique :db/valueType
+                          :db.alter/attribute
+                          :db.bootstrap/part
+                          :db.cardinality/many :db.cardinality/one
+                          :db.excise/attrs :db.excise/before :db.excise/beforeT
+                          :db.fn/cas :db.fn/retractEntity
+                          :db.install/attribute :db.install/function :db.install/partition :db.install/valueType
+                          :db.lang/clojure :db.lang/java
+                          :db.part/db :db.part/tx :db.part/user
+                          :db.sys/partiallyIndexed :db.sys/reId
+                          :db.type/bigdec :db.type/bigint :db.type/boolean :db.type/bytes :db.type/double
+                          :db.type/float :db.type/fn :db.type/instant :db.type/keyword :db.type/long :db.type/ref
+                          :db.type/string :db.type/uri :db.type/uuid
+                          :db.unique/identity :db.unique/value
+                          :fressian/tag})
 
 (defn- attributes [db]
   (map :v (d/datoms db :avet :db/ident)))
