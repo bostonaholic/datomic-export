02d716dd47a2fa88cd380494bce41f01cdaead9c
Matthew Boston <matthew.boston@gmail.com>
2016-03-21 16:28:13 -0600

    FIX: forgot to add data.csv to dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.3"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5350"]]}
              :test {:dependencies [[com.datomic/datomic-free "0.9.5350"]]}}
   :aliases {"db:export" ["run" "-m" datomic-export.core]})
